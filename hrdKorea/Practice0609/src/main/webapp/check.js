abad652cf844317732d1fe6efd0c2d20a46a8b26
Rheehose <sea35652@gmail.com>
2026-06-23 15:03:34 +0900

    문제를 해결했다

--- hrdKorea/Practice0609/src/main/webapp/check.js
+++ hrdKorea/Practice0609/src/main/webapp/check.js
@@ -1,25 +1,21 @@
 function chk(){
-	if(documen.frm.do_date.value==""){
+	if(document.frm.do_date.value==""){
 		alert("일자가 등록되지 않았습니다.");
 		document.frm.do_date.focus();
-		return false
+		return false;
 	}
-	if(documen.frm.do_date.value==""){
+	if(document.frm.fk_member.value==""){
 		alert("회원번호를 입력하지 않고 무엇했느냐. 다시하거라.");
 		document.frm.fk_member.focus();
-		return false
+		return false;
 	}
-	if(documen.frm.do_date.value==""){
+	if(document.frm.do_memo.value==""){
 		alert("메모를 입력하거라.");
 		document.frm.do_memo.focus();
-		return false
+		return false;
 	}
+	return true;
 }
-
-
-
-
-
 
 
 /** I HAVE A PEN, I HAVE AN APLLE. OH! APPLE PEN. */
